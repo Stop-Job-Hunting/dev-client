bf735165cae28d3220b61f7ea65c6fe0b89f0ca4
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-06-02 17:17:23 -0700

    fixed nav bar styling

--- components/navBar.js
+++ components/navBar.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import LogoutButton from "../components/logoutButton";
 import { useRouter } from "next/router";
-import SERVERURL from "../constants"
+import SERVERURL from "../constants";
 
 // defines the fetch request to server to check if logged in
 function getLoggedIn() {
@@ -60,20 +60,20 @@ function NavBar() {
           </div>
           <LogoutButton />
           <style jsx>{`
-    .builderLogoutContainer {
-      display: flex;
-      flex- direction: row;
-  align - items: center;
-  justify - content: center;
-}
+            .builderLogoutContainer {
+              display: flex;
+              flex-direction: row;
+              align-items: center;
+              justify-content: center;
+            }
             .builderContainer {
-  margin - right: 1em;
-  cursor: pointer;
-}
-            .builderContainer: hover {
-  opacity: 0.7;
-}
-`}</style>
+              margin-right: 1em;
+              cursor: pointer;
+            }
+            .buildercontainer: hover {
+              opacity: 0.7;
+            }
+          `}</style>
         </div>
       );
     } else if (navState === "loggedOut") {
@@ -98,38 +98,38 @@ function NavBar() {
           </div>
 
           <style jsx>{`
-  .logInContainer {
-  margin - right: 1em;
-  cursor: pointer;
-}
-            .logInContainer: hover {
-  opacity: 0.7;
-}
+            .logInContainer {
+              margin-right: 1em;
+              cursor: pointer;
+            }
+            .logincontainer: hover {
+              opacity: 0.7;
+            }
             .inOutContainer {
-  display: flex;
-  align - items: center;
-  justify - content: center;
-  flex - direction: row;
-}
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              flex-direction: row;
+            }
             .buttonContainer {
-  display: flex;
-  align - items: center;
-  justify - content: center;
-  height: 2.2em;
-  width: 5em;
-  padding - left: 0.5em;
-  padding - right: 0.5em;
-  border: 4px solid #ffa600;
-  border - radius: 4px;
-  background - color: #ffa600;
-  color: white;
-  font - weight: bold;
-  cursor: pointer;
-}
-            .buttonContainer: hover {
-  opacity: 0.7;
-}
-`}</style>
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              height: 2.2em;
+              width: 5em;
+              padding-left: 0.5em;
+              padding-right: 0.5em;
+              border: 4px solid #ffa600;
+              border-radius: 4px;
+              background-color: #ffa600;
+              color: white;
+              font-weight: bold;
+              cursor: pointer;
+            }
+            .buttoncontainer: hover {
+              opacity: 0.7;
+            }
+          `}</style>
         </div>
       );
     }
@@ -149,26 +149,26 @@ function NavBar() {
       </div>
       <div>{loggedStatus}</div>
       <style jsx>{`
-  .navContainer {
-  display: flex;
-  align - items: center;
-  justify - content: space - between;
-  padding - left: 1em;
-  padding - right: 1em;
-  height: 4.5em;
-  background - color: #fbfcfd;
-  box - shadow: 2px 2px 2px rgba(0, 0, 0, 0.16);
-}
+        .navContainer {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          padding-left: 1em;
+          padding-right: 1em;
+          height: 4.5em;
+          background-color: #fbfcfd;
+          box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.16);
+        }
         .logoContainer {
-  font - size: 2em;
-  font - weight: bold;
-  color: #444;
-  cursor: pointer;
-}
-        .logoContainer: hover {
-  opacity: 0.9;
-}
-`}</style>
+          font-size: 2em;
+          font-weight: bold;
+          color: #444;
+          cursor: pointer;
+        }
+        .logocontainer: hover {
+          opacity: 0.9;
+        }
+      `}</style>
     </div>
   );
 }
