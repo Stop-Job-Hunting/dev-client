7f5b2a6b7bb6ba36e8ce656362aea4d35f399860
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-06-02 17:14:26 -0700

    fixed constants

--- constants.js
+++ constants.js
@@ -1,3 +1,6 @@
-const SERVERURL = process.env.NODE_ENV === "production" ? "https://sjh-server.herokuapp.com" : "localhost:3001"
+const SERVERURL =
+  process.env.NODE_ENV === "production"
+    ? "https://sjh-server.herokuapp.com"
+    : "http://localhost:3001";
 
-export default SERVERURL
+export default SERVERURL;
