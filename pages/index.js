5af93e0f500b70dfb0c7b1184984cc7a64139bd0
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-06-08 20:45:00 -0700

    landing page is basically done minus icons

--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,13 @@ export default function Home() {
           <img src="/assets/resume-example.png" width="320px"></img>
         </div>
       </div>
+
+      {/* Call to action */}
       <div className="callToActionBarContainer">
         Templates Proven To Help Get You Hired
       </div>
+
+      {/* How It Works */}
       <div className="howItWorksContainer">
         <div className="hiwTitleContainer">How It Works</div>
         <div className="hiwSubItemsContainer">
@@ -56,6 +60,38 @@ export default function Home() {
         </div>
       </div>
 
+      {/* Review Section */}
+      <div className="reviewContainer">
+        <div className="imgContainerReviews">
+          <img className="heroImage" src="/assets/reviewbanner.png"></img>
+        </div>
+        <div className="reviewHeaderContainer">What are our users saying?</div>
+
+        <div className="reviewImageContainer">
+          <img
+            className="reviewImage"
+            src="/assets/sammilittle.png"
+            width="300px"
+          ></img>
+          <img
+            className="reviewImageSecond"
+            src="/assets/ryanclark.png"
+            width="300px"
+          ></img>
+          <img
+            className="reviewImagelast"
+            src="/assets/tylerrutzen.png"
+            width="300px"
+          ></img>
+        </div>
+      </div>
+      <div className="lastCallToActionContainer">
+        <div className="lastCallToActionHeader">Ready To Get Started?</div>
+        <div className="buttonContainerLastCTA">Build My Resume</div>
+      </div>
+      <div className="footerContainer">
+        Contact | Privacy Policy | Terms Of Service
+      </div>
       <style jsx>{`
         .indexContainer {
           display: flex;
@@ -67,6 +103,12 @@ export default function Home() {
           position: absolute;
           z-index: -1;
           height: 550px;
+          width: 100%;
+        }
+        .imgContainerReviews {
+          position: absolute;
+          z-index: -1;
+          height: 400px;
           width: 100%;
         }
         .overlayContainer {
@@ -125,7 +167,6 @@ export default function Home() {
         }
         .howItWorksContainer {
           width: 100%;
-          border-bottom: 10px solid red;
           height: 400px;
           display: flex;
           align-items: center;
@@ -158,8 +199,95 @@ export default function Home() {
           font-size: 18px;
           margin: 15px;
         }
+        .reviewContainer {
+          width: 100%;
+          min-height: 400px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-direction: column;
+        }
+        .reviewImage {
+          margin: 30px 10px 10px 10px;
+        }
+        .reviewImagelast {
+          margin: 30px 10px 10px 10px;
+        }
+        .reviewImageSecond {
+          margin: 30px 10px 10px 10px;
+        }
+        .reviewHeaderContainer {
+          font-size: 40px;
+          color: white;
+          font-weight: bold;
+          margin-top: 10px;
+        }
+
+        .lastCallToActionContainer {
+          width: 100%;
+          background-color: #444;
+          height: 300px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          color: white;
+          font-size: 22px;
+          font-weight: bold;
+          flex-direction: column;
+        }
+
+        .buttonContainerLastCTA {
+          outline: 5px solid white;
+          width: 200px;
+          height: 50px;
+          font-size: 20px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          margin-top: 50px;
+          cursor: pointer;
+          color: #white;
+        }
+        .buttonContainerLastCTA:hover {
+          opacity: 0.7;
+          cursor: pointer;
+        }
+
+        .lastCallToActionHeader {
+          font-size: 40px;
+          color: white;
+          font-weight: bold;
+          margin-top: 10px;
+        }
+
+        .footerContainer {
+          width: 100%;
+          background-color: #444;
+          color: white;
+          font-size: 14px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          height: 40px;
+        }
+
         @media only screen and (max-width: 820px) {
           .resumePhotoContainer {
+            display: none;
+          }
+          .reviewImagelast {
+            display: none;
+          }
+        }
+
+        @media only screen and (max-width: 960px) {
+          .reviewImagelast {
+            display: none;
+          }
+        }
+
+        @media only screen and (max-width: 640px) {
+          .reviewImageSecond {
             display: none;
           }
         }
