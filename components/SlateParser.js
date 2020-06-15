7f80be1981d75f1dedfeb291c46845ae628da97d
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-06-15 11:45:35 -0700

    built slate parser

---
+++ components/SlateParser.js
@@ -0,0 +1,67 @@
+// const SlateArr = [
+//   {
+//     type: "bulleted-list",
+//     children: [
+//       {
+//         type: "list-item",
+//         children: [{ text: "Write your work highlights here!" }],
+//       },
+//       {
+//         type: "list-item",
+//         children: [
+//           { text: "This is your second Bullet Point!" },
+//           { text: "But wait! I'm not done typing yet!" },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     type: "paragraph",
+//     children: [
+//       { text: "This is the start of my paragraph. " },
+//       { text: "But wait! Im not done typing it quite yet." },
+//     ],
+//   },
+// ];
+
+// Check if bulleted-list, list item, or paragraph
+
+// if bulleted-list, call function on children.
+// if list-item, call function on children.
+
+function SlateParser(slateArr) {
+  let answerObj = {
+    highlights: [],
+    paragraph: "",
+  };
+
+  // Loop through slateArr children
+  for (let i = 0; i < slateArr.length; i++) {
+    // check to see if bulleted list or paragraph
+    if (slateArr[i].type === "bulleted-list") {
+      // loop through the <li> of the bulleted list
+      for (let x = 0; x < slateArr[i].children.length; x++) {
+        // make sure it's a list item
+        if (slateArr[i].children[x].type === "list-item") {
+          // loop through the list item, print the text
+          let childText = "";
+          for (let y = 0; y < slateArr[i].children[x].children.length; y++) {
+            // push it into our return variable
+            childText = childText + slateArr[i].children[x].children[y].text;
+          }
+          answerObj.highlights.push(childText);
+        }
+      }
+    } else if (slateArr[i].type === "paragraph") {
+      let paragraphText = "";
+      // loop through the paragraph children
+      for (let z = 0; z < slateArr[i].children.length; z++) {
+        paragraphText = paragraphText + slateArr[i].children[z].text;
+      }
+    }
+  }
+
+  return answerObj;
+}
+
+export default SlateParser;
