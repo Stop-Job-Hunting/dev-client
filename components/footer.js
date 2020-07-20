33913a46cf2641611e2870e757f8abe0f42be4fd
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-07-20 16:05:18 -0700

    put GA tag in footer, poorly, needs fixed

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,29 @@
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 function Footer() {
   const router = useRouter();
 
   return (
     <div className="footerContainer">
+      <Head>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `
+                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-167272769-3"></script>
+                <script>
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                
+                  gtag('config', 'UA-167272769-3');
+                </script>
+                `,
+          }}
+        ></div>
+      </Head>
+
+      {/* <ScriptTag type="text/javascript" src="./ga.js" /> */}
       <div className="linkItemContainer">
         Copyright © 2020 StopJobHunting.com &nbsp;| &nbsp;
         <div
